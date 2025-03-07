da7bda31adb036a0c0d33b26552a0fe47303d656
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-03-07 08:02:39 -0300

    :sparkles: feat: Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "mongodb+srv://tatyanegoncalves023:esse_ano_vai_ser_incrivel@clients.klksc.mongodb.net/clients?retryWrites=true&w=majority&appName=Clients"
+  baseURL: "https://gerenciador-clientes-back.vercel.app/"
 })
